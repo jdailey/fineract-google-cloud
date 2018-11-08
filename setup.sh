207ec5b3d9ff74881b35154ae3e8b98a63494e23
James <jamespdailey@gmail.com>
2018-11-08 09:33:48 -0800

    Setup script initial test checkin

---
+++ setup.sh
@@ -0,0 +1,8 @@
+* Commands to be issued in Google Cloud shell 
+* this is empheral so, each session starts by cloning this repo 
+* and running the repo 
+
+echo "Script Running, which java: " 
+java -version 
+
+
